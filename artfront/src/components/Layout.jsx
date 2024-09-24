45d3f02725b8e45db6d8384491f4693cab94f30f
lusimba <newville92@gmail.com>
2024-09-24 16:08:32 +0300

    Add URL

--- artfront/src/components/Layout.jsx
+++ artfront/src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useLocation, Outlet, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logoutUser } from '../store/slices/authSlice';
@@ -28,6 +28,10 @@ function Layout() {
         }
         return location.pathname.startsWith(path);
     };
+
+    useEffect(() => {
+        console.log( {URL} )
+    }, [location]);
 
     const handleLogout = () => {
         dispatch(logoutUser()).then(() => {
